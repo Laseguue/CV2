3645ced12b7b28a172248a9ea82a4a4bed9102d7
laseguue <ar.seguret@gmail.com>
2023-10-21 11:43:53 +0200

    Ajout d'un lien de téléchargement pour le CV

--- cv/models.py
+++ cv/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
-from .forms import ContactForm
+from .forms import cf
+
 
 class PersonalInfo(models.Model):
     photo = models.ImageField(upload_to='photos/', max_length=500)
@@ -11,6 +12,8 @@ class PersonalInfo(models.Model):
     linkedin = models.URLField(max_length=255)
     github = models.URLField(max_length=255)
     summary = models.TextField(blank=True)
+    cv_url = models.URLField(blank=True, null=True, verbose_name="CV URL", max_length=500)
+
 
 class Education(models.Model):
     diploma = models.CharField(max_length=255)
@@ -18,6 +21,7 @@ class Education(models.Model):
     year = models.IntegerField()
     image = models.ImageField(upload_to='education_images/', max_length=500)
     is_ongoing = models.BooleanField(default=False)
+
 
 class Experience(models.Model):
     position = models.CharField(max_length=255)
@@ -27,10 +31,12 @@ class Experience(models.Model):
     tasks = models.TextField()
     diplome_image = models.ImageField(upload_to='diplomes/', null=True, blank=True, max_length=500)
 
+
 class Skill(models.Model):
     name = models.CharField(max_length=255)
     level = models.IntegerField()
     certificate_image = models.ImageField(upload_to='skill_images/', max_length=500)
+
 
 class PythonProject(models.Model):
     title = models.CharField(max_length=255)
--- cv/views.py
+++ cv/views.py
@@ -3,33 +3,40 @@ from .models import PersonalInfo, Education, Experience, Skill, PythonProject
 from django.core.mail import send_mail
 from .forms import ContactForm
 
+
 def home(request):
     personal_info = PersonalInfo.objects.first()
     return render(request, 'cv/home.html', {'personal_info': personal_info})
 
+
 def about(request):
     personal_info = PersonalInfo.objects.first()
     return render(request, 'cv/about.html', {'personal_info': personal_info})
+
 
 def experience(request):
     experiences = Experience.objects.all()
     personal_info = PersonalInfo.objects.first()
     return render(request, 'cv/experience.html', {'experiences': experiences, 'personal_info': personal_info})
 
+
 def experience_detail(request, experience_id):
     experience = get_object_or_404(Experience, id=experience_id)
     personal_info = PersonalInfo.objects.first()
     return render(request, 'cv/experience_detail.html', {'experience': experience, 'personal_info': personal_info})
+
 
 def skills(request):
     skills = Skill.objects.all()
     personal_info = PersonalInfo.objects.first()
     return render(request, 'cv/skills.html', {'skills': skills, 'personal_info': personal_info})
 
+
 def skill_detail(request, skill_id):
     skill = get_object_or_404(Skill, id=skill_id)
     personal_info = PersonalInfo.objects.first()
     return render(request, 'cv/skill_detail.html', {'skill': skill, 'personal_info': personal_info})
+
 
 def contact(request):
     if request.method == 'POST':
@@ -39,15 +46,13 @@ def contact(request):
             email = form.cleaned_data['email']
             phone = form.cleaned_data['phone']
             message = form.cleaned_data['message']
-
-            # Formatage du corps de l'email pour inclure toutes les informations
             email_body = f"Nom: {name}\nEmail: {email}\nTéléphone: {phone}\n\nMessage:\n{message}"
 
             send_mail(
-                f'Contact de {name}',  # Objet de l'email
-                email_body,  # Corps de l'email
-                email,  # Expéditeur
-                ['ghostdogthe1@gmail.com'],  # Destinataire
+                f'Contact de {name}',
+                email_body,
+                email,
+                ['ghostdogthe1@gmail.com'],
             )
 
             return redirect('success')
